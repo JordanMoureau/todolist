556e9060a9ca8384176df2d26d84ad5f6df94019
Jordan Moureau <jordanmoureau@Jordans-iMac.local>
2023-06-08 22:36:21 -0700

    all the codes

---
+++ script.js
@@ -0,0 +1,51 @@
+let addToDoButton = document.getElementById('addToDo');
+let longTermList = document.getElementById('longTermContainer');
+let inputField = document.getElementById('inputField');
+let todayList = document.getElementById('todayContainer');
+
+addToDoButton.addEventListener('click', function() {
+    var newLabel = document.createElement("label");
+
+    newLabel.classList.add('todo-container');
+    longTermList.appendChild(newLabel);
+    
+    var newCheckBox = document.createElement("input");
+
+    newCheckBox.type = "checkbox";
+    newLabel.appendChild(newCheckBox);
+
+    var newSpan = document.createElement("span");
+    newSpan.classList.add("checkmark");
+    newLabel.appendChild(newSpan);
+    
+    var paragraph = document.createElement('p');
+
+    paragraph.innerText = inputField.value;
+    newLabel.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('dblclick', function(){
+        newLabel.removeChild(paragraph);
+        newLabel.removeChild(newCheckBox);
+        newLabel.removeChild(newSpan);
+        longTermList.removeChild(newLabel);
+    })
+    
+})
+
+var input = document.getElementById("inputField");
+
+input.addEventListener('keypress', function(event) {
+    if (event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("addToDo").click();
+    }
+});
+
+
+
+function appendChildClass(newElement, elementType, elementClass, parent) {
+    newElement = document.createElement(elementType);
+
+    newElement.classList.add(elementClass);
+    parent.appendChild(newLabel);
+}
