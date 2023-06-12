cf140442d5d09c8fdc4bdb7cd3b9496bde56dd52
Jordan Moureau <jordanmoureau@Jordans-iMac.local>
2023-06-11 22:45:26 -0700

    all the lists work now

--- script.js
+++ script.js
@@ -1,52 +1,93 @@
 let addToDoButton = document.getElementById('addToDo');
 let longTermList = document.getElementById('longTermContainer');
 let inputField = document.getElementById('inputField');
+
+//variables for today todo list
+
 let todayList = document.getElementById('todayContainer');
+let todayInput = document.getElementById('inputFieldToday')
+let todayAdd = document.getElementById('todayAdd')
+
+//variables for this week todo list
+
+let weekList = document.getElementById('weekContainer');
+let weekInput = document.getElementById('inputFieldWeek')
+let weekAdd = document.getElementById('weekAdd')
 
 addToDoButton.addEventListener('click', function() {
-    var newLabel = document.createElement("label");
+   
+    createAddListItem("label", "todo-container", longTermList, inputField);
 
-    newLabel.classList.add('todo-container');
-    longTermList.appendChild(newLabel);
+})
 
-    
+todayAdd.addEventListener('click', function() {
+   
+    createAddListItem("label", "todo-container-today", todayList, todayInput);
+
+})
+
+weekAdd.addEventListener('click', function() {
+   
+    createAddListItem("label", "todo-container-today", weekList, weekInput);
+
+})
+
+
+
+function createAddListItem(elementType, elementClass, parent, input) {
+    let newElement = createElementWrapper(elementType, elementClass, parent);
+    console.log(newElement);
+    newListItem(newElement, input, parent);
+}
+
+addEnter(inputField, addToDoButton);
+addEnter(todayInput, todayAdd);
+addEnter(weekInput, weekAdd);
+
+
+function newListItem(parent, input, parentsParent) {
     var newCheckBox = document.createElement("input");
 
     newCheckBox.type = "checkbox";
-    newLabel.appendChild(newCheckBox);
+    parent.appendChild(newCheckBox);
 
     var newSpan = document.createElement("span");
     newSpan.classList.add("checkmark");
-    newLabel.appendChild(newSpan);
+    parent.appendChild(newSpan);
     
     var paragraph = document.createElement('p');
 
-    paragraph.innerText = inputField.value;
-    newLabel.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('dblclick', function(){
-        newLabel.removeChild(paragraph);
-        newLabel.removeChild(newCheckBox);
-        newLabel.removeChild(newSpan);
-        longTermList.removeChild(newLabel);
-    })
-    
-})
-
-var input = document.getElementById("inputField");
-
-input.addEventListener('keypress', function(event) {
-    if (event.key === "Enter"){
-        event.preventDefault();
-        document.getElementById("addToDo").click();
+    paragraph.innerText = input.value;
+    if (paragraph.innerText === "") {
+        paragraph.innerText = "Tha's a nothing. Double click to delete me.";
     }
-});
+    
+    parent.appendChild(paragraph);
+    input.value = "";
+    paragraph.addEventListener('dblclick', function(){
+        parent.removeChild(paragraph);
+        parent.removeChild(newCheckBox);
+        parent.removeChild(newSpan);
+        parentsParent.removeChild(parent);
+    })
+}
 
-
-
-function appendChildClass(elementType, elementClass, parent) {
-    let newElement = document.createElement(elementType);
+function createElementWrapper(elementType, elementClass, parent) {
+    var newElement = document.createElement(elementType);
 
     newElement.classList.add(elementClass);
     parent.appendChild(newElement);
+
+    return newElement;
+}
+
+
+function addEnter(input, buttonId) {
+
+    input.addEventListener('keypress', function(event) {
+        if (event.key === "Enter"){
+            event.preventDefault();
+            buttonId.click();
+        }
+    });
 }
